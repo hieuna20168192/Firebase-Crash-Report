9962ddf764493294aa20d8e470bf0e5ed52991f2
hieuna20168192 <Giadinhtoi76@>
2023-01-04 01:44:06 +0700

    encounter build variants

---
+++ app/src/full/java/Logging.kt
@@ -0,0 +1,1 @@
+data class Logging(val msg: String = "")
---
+++ app/src/testMinApi23/java/SampleTest.kt
@@ -0,0 +1,12 @@
+import com.bumptech.glide.Glide
+import org.junit.Test
+import org.junit.Assert.*
+
+class SampleTest {
+
+    @Test
+    fun isAdd_correct() {
+        assertEquals(4, 2 + 7)
+        // Glide
+    }
+}
